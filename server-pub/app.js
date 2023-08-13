1203aadf10762fea92d534e9c47fefb71055eb4c
dcortesnet <dcortes.net@gmail.com>
2023-08-13 17:52:03 -0400

    Publish message queue in rabbitMQ

--- server-pub/app.js
+++ server-pub/app.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const app = express();
+const { v4 } = require('uuid');
 const PORT = 3000;
+const amqp = require('amqplib');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+let rabbitMQConnection;
 
-app.get('/', (req, res) => {
-  return res.send("Hello world!");
+app.post('/messages', async (req, res) => {
+  try {
+
+    if (!req.body?.message) {
+      return res.status(400).json({
+        detail: "The message property is required"
+      });
+    }
+
+    const message = {
+      id: v4(),
+      message: req.body.message,
+      date: new Date(),
+    };
+
+    const channel = await rabbitMQConnection.createChannel();
+    const queueName = v4();
+    await channel.assertQueue(queueName, { durable: false });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
+    console.log(`Publishing an Event using RabbitMQ to :${req.body.message}`);
+    await channel.close();
+    return res.json({
+      detail: 'Publishing an Event using RabbitMQ successful',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      detail: error.message
+    });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(` 😀 server on port ${PORT}  `);
+amqp.connect('amqp://localhost').then(connection => {
+  rabbitMQConnection = connection;
+  app.listen(PORT, () => {
+    console.log(` 😀 server on port ${PORT}  `);
+  });
 });
+
